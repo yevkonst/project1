3ca5f2a03a2f1cfd3efef296fbabdcd569d9a4bc
Евгений <yevkonst.dev@gmail.com>
2020-05-20 12:52:49 +0300

    dmx-office

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ЖОПА ПОЛНАЯ
+                    ЖОПА ПОЛНАЯ 2
                 </div>
 
                 <div class="links">
